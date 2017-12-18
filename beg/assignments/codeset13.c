d59ec0c10812ccc8240d8c77f093a3955c967e08
revakrish266 <revathikrishnan266@gmail.com>
2017-12-18 21:58:03 +0530

    Create codeset13.c

---
+++ beg/assignments/codeset13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+char s1;
+scanf("%s",s1);
+if(s1=='A'||s1=='E'||s1=='I'||s1=='O'||s1=='U'||s1=='a'||s1=='e'||s1=='i'||s1=='o'||s1=='u')
+{
+printf("vowel");
+}
+else
+{
+printf("consonant");
+}
+}
