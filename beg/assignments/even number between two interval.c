97be121566fb0f7d736c03b5420dfe6a2fe9179f
revakrish266 <revathikrishnan266@gmail.com>
2017-12-19 21:04:31 +0530

    Add files via upload

---
+++ beg/assignments/even number between two interval.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) 
+
+{
+
+int start,end,i=0;
+
+printf("enter the  starting number");
+
+scanf("%d",&start);
+
+printf("enter the ending number");
+
+scanf("%d",&end);
+
+for(i=start+1;i<=end;i++)
+
+{
+
+if(i%2==0)
+
+{
+
+printf("%d",i);
+
+}
+
+}
+
+return 0;
+
+}
+
