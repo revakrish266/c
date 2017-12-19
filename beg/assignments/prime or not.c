4995e217eb7ce00f2c147ae9475552fd66629d27
revakrish266 <revathikrishnan266@gmail.com>
2017-12-19 20:29:54 +0530

    Add files via upload

--- beg/assignments/prime or not.c
+++ beg/assignments/prime or not.c
@@ -1,23 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 int main(void)
 {
-int n,m,i,flag=0;
-printf(" enter the number");
+
+int n,m,i,flag=0;
+	
+printf("enter the number");
+	
 scanf("%d",&n);
+
 m=n/2;
+	
 for(i=2;i<=m;i++)
+	
 {
+
 if(n%i==0)
+		
 {
-printf("not prime");
+			
+printf("not a prime number %d",n);
+			
 flag=1;
+		
 }
+	
 }
+	
 if(flag==0)
+	
 {
+		
 printf("prime number %d",n);
+	
 }
+	
+	
+return 0;
+
 }
-
-
 
