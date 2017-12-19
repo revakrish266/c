3a76b607d891710501700f2e0cbf8912035d2a81
revakrish266 <revathikrishnan266@gmail.com>
2017-12-19 21:36:51 +0530

    Add files via upload

---
+++ beg/assignments/prime number between two intervals.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+int n,m,i,flag=0;
+	
+printf("enter the number");
+	
+scanf("%d%d",&n,&m);
+
+while(n<m)
+{	
+for(i=2;i<=n/2;i++)
+	
+{
+
+flag=0;
+if(n%i==0)
+		
+{
+			
+
+			
+flag=1;
+		
+}
+	
+}
+	
+if(flag==0)
+	
+{
+		
+printf(" %d",n);
+	
+}
+	
+	
+++n;
+}
+return 0;
+
+}
+
