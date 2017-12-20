04c583dbb7ae98c735d01843bbe7e8c93b40d22e
revakrish266 <revathikrishnan266@gmail.com>
2017-12-20 22:38:34 +0530

    Add files via upload

---
+++ assessmentprograms/array rotation.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+using namespace std;
+
+
+int main() 
+{
+int n,r,a[n],i;
+
+printf("enter the array size");
+
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+
+{
+    
+scanf("%d",&a[i]);
+
+}
+
+printf("enter the no of rotation");
+
+scanf("%d",&r);
+
+r=r%n;
+
+for(i=r;i<n;i++)
+{
+    
+printf("%d",a[i]);
+
+}
+
+for(i=0;i<r;i++)
+
+{
+    
+printf("%d",a[i]);
+
+}
+	
+return 0;
+
+}
