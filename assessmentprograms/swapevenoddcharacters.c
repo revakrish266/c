380e30d950979d354796e81029eacb319b2287ab
revakrish266 <revathikrishnan266@gmail.com>
2017-12-20 14:50:54 +0530

    Add files via upload

---
+++ assessmentprograms/swapevenoddcharacters.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+int i,j,t,len;
+char str[10];
+scanf("%s",str);
+len=strlen(str);
+if(len%2==0)
+{
+for(i=0;i<=len;i+=2)
+{
+t=str[i];
+str[i]=str[i+1];
+str[i+1]=t;
+}
+printf("%s",str);
+}
+else
+{
+for(i=0;i<len;i+=2)
+{
+t=str[i];
+str[i]=str[i+1];
+str[i+1]=t;
+}
+printf("%s",str);
+}
+}
