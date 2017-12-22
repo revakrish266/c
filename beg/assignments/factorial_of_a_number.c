6e8249ab555a93ffed6207d189bf45f9e249d3e3
revakrish266 <revathikrishnan266@gmail.com>
2017-12-22 20:06:22 +0530

    Add files via upload

---
+++ beg/assignments/factorial_of_a_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int n,i,fact=1;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    fact=fact*i;
+}
+printf(" factorial of a %d is %d",n,fact);
+	return 0;
+}
+
