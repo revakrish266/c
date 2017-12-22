b45ed4dcde521fa8685bb0e04f8d58550ca96705
revakrish266 <revathikrishnan266@gmail.com>
2017-12-22 20:07:30 +0530

    Add files via upload

---
+++ beg/assignments/multiples_of_n_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+int n,m=1,i;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=5;i++)
+{
+    printf("\n %d * %d = %d",i,n,i*n);
+}
+
+
+	return 0;
+}
+
