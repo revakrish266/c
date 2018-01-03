c2cc99fd0fa88cfbf00c84433579d98530b84c87
revakrish266 <revathikrishnan266@gmail.com>
2018-01-03 22:09:24 +0530

    Add files via upload

---
+++ beg/assignments/arithmeticprogression.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) 
+
+{
+
+int n,d,f,sum=0,a=0,i;
+
+printf("\n enter the no of terms");
+
+scanf("%d",&n);
+
+printf("enter the difference");
+
+scanf("%d",&d);
+
+printf("\n enter the first number");
+
+scanf("%d",&f);
+
+for(i=0;i<n;i++)
+
+{
+    
+a=a+d;
+ 
+   sum=sum+a;
+ 
+   
+}
+
+printf("%d",sum);
+
+ return 0;
+
+}
+
