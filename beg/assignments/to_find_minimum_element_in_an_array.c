2d50048dd9bb22d750d372c3fc767d2b3c062d0c
revakrish266 <revathikrishnan266@gmail.com>
2018-01-09 12:49:05 +0530

    Add files via upload

---
+++ beg/assignments/to_find_minimum_element_in_an_array.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+	
+
+	int arr[15],i,n,j,min;
+
+	scanf("%d",&n);
+
+	for(i=0;i<n;i++)
+
+	{
+	  
+  scanf("%d",&arr[i]);
+
+	   
+	}
+	
+min=arr[0];
+
+	for(i=1;i<n;i++)
+
+	{
+	
+        if(min>arr[i])
+
+	        {
+
+	            min=arr[i];
+
+	        }
+
+	   	}
+	 
+  	printf("%d",min);
+
+	return 0;
+
+}
+
