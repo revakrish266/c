5683d0f15321740f04bd64a4e2deac58b3336593
revakrish266 <revathikrishnan266@gmail.com>
2018-01-10 21:28:20 +0530

    Add files via upload

---
+++ assessmentprograms/print_the_largest_number_without_repeating_values.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+
+int n,j,i,temp,a[19];
+
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+
+{
+
+    scanf("%d",&a[i]);
+
+}
+
+for(i=0;i<n;i++)
+
+{
+
+    for(j=i+1;j<n;j++)
+
+    {
+
+        if(a[i]<a[j])
+
+        {
+
+            temp=a[i];
+
+            a[i]=a[j];
+ 
+           a[j]=temp;
+
+        }
+ 
+   }
+
+}
+
+for(i=0;i<n;i++)
+
+{
+
+    if(a[i]!=a[i+1])
+
+    {
+  
+      temp1=a[i];
+ 
+   printf("%d ",a[i]);
+    
+    }
+
+    
+}
+
+
+	return 0;
+
+}
+
