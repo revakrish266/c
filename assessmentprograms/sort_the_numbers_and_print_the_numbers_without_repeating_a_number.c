faf54f43978226249acb39ba727532027e2c55dd
revakrish266 <revathikrishnan266@gmail.com>
2018-01-10 21:29:17 +0530

    Add files via upload

---
+++ assessmentprograms/sort_the_numbers_and_print_the_numbers_without_repeating_a_number.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+
+
+int arr[15],n,j,i,temp,temp1;
+
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+
+{
+
+    scanf("%d",&arr[i]);
+
+    }
+
+    for(i=0;i<n;i++)
+
+    {
+ 
+       for(j=i+1;j<n;j++)
+
+        {
+ 
+           if(arr[i]>arr[j])
+
+            {
+    
+            temp=arr[i];
+
+              arr[i]=arr[j];
+
+                arr[j]=temp;
+ 
+           }
+  
+      }
+
+    }
+  
+  for(i=0;i<n;i++)
+  
+  {
+ 
+       if(arr[i]!=arr[i+1])
+
+        {
+
+         temp1=arr[i];
+  
+       printf("%d",temp1);
+
+        }
+
+    }
+ 
+   
+	return 0;
+
+}
+
+ 
