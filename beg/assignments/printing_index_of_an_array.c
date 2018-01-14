82ce5daeab0e6782bbf6a7b5a53173dbb6221409
revakrish266 <revathikrishnan266@gmail.com>
2018-01-14 11:54:12 +0530

    Add files via upload

---
+++ beg/assignments/printing_index_of_an_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+
+
+int n,arr[20],i,j;
+
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+
+{
+
+    scanf("%d",&arr[i]);
+
+}
+
+for(i=0;i<n;i++)
+
+{
+
+    printf("\n %d %d",arr[i],i);
+
+}
+	
+return 0;
+
+}
+
