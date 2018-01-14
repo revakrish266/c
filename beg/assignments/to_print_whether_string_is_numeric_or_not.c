ea95d64201c04bfb545b49909ad617961fe4728a
revakrish266 <revathikrishnan266@gmail.com>
2018-01-14 11:43:03 +0530

    Add files via upload

---
+++ beg/assignments/to_print_whether_string_is_numeric_or_not.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#include<string.h>
+
+
+int main(void)
+ {
+	
+char str[12];
+
+int len,i,n,flag=0;
+
+scanf("%s",str);
+
+len=strlen(str);
+
+for(i=0;i<len;i++)
+
+{
+
+    if(str[i]>='0'&&str[i]<='9')
+
+    {
+ 
+      flag=1;
+
+       
+
+    }
+
+    else
+
+    {
+ 
+       
+        printf("NO");
+
+    }
+
+}
+
+if(flag==1)
+
+{
+
+    printf("YES");
+
+}
+	
+return 0;
+
+}
+
