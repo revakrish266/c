09856a8db2b0e754cfac3eedd2959667211a547a
revakrish266 <revathikrishnan266@gmail.com>
2018-01-16 16:20:11 +0530

    Add files via upload

---
+++ beg/assignments/counting_number_of_words_in_given_line.java
@@ -0,0 +1,38 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+class Codechef
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+
+	{
+	 
+ String a;
+
+      int count=0;
+
+      Scanner s=new Scanner(System.in);
+
+      a=s.nextLine();
+
+     String d[]=a.split("\\s+");
+ 
+    for(int i=0;i<d.length;i++)
+{
+ 
+        count++;
+
+     }
+
+     System.out.println(count);
+    
+}
+
+	
+}
