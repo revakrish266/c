0fa995a75f61453b32471cdbf4be7ab54d31a013
revakrish266 <revathikrishnan266@gmail.com>
2018-01-18 23:25:09 +0530

    Create to_find_difference_of_two_number_odd_or_even.c

---
+++ beg/assignments/to_find_difference_of_two_number_odd_or_even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+int n,m,i,j;
+scanf("%d %d",&n,&m);
+j=n-m;
+if(j%2==0)
+{
+printf("even number %d",j);
+}
+else
+{
+printf(" odd number %d",j);
+}
+}
+© 2018 GitHub, Inc.
