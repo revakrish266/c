79c56665415f2e5b05a0e5fef9b5db80bc6d06c6
revakrish266 <revathikrishnan266@gmail.com>
2018-01-18 20:48:10 +0530

    Create biggest_element_in_an_array.c

---
+++ beg/assignments/biggest_element_in_an_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) 
+{
+int arr[15],i,n,j,max;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+scanf("%d",&arr[i]);
+	}
+max=arr[0];
+	for(i=1;i<n;i++)
+	{
+      if(max<arr[i])
+	        {
+            max=arr[i];
+	        }
+	   	}
+	printf("%d",max);
+	return 0;
+}
