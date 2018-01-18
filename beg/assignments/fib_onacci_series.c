4079f7a83c5c88917972b2fa2e3bf73095fcb9b4
revakrish266 <revathikrishnan266@gmail.com>
2018-01-18 20:51:56 +0530

    Create fib_onacci_series.c

---
+++ beg/assignments/fib_onacci_series.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+int n,a=0,b=1,i,j,count;
+scanf("%d",&n);
+printf("%d",b);
+coount=1;
+while(count<n)
+{
+i=a+b;
+count++;
+printf("\n %d",i);
+a=b;
+b=i;
+}
+}
