8f9df7643f334b8ad3291c81720f9c4c19aa5bb3
revakrish266 <revathikrishnan266@gmail.com>
2018-01-18 21:34:48 +0530

    Create given_string_s_and_number_k_to_print_first_k_characters.java

---
+++ beg/assignments/given_string_s_and_number_k_to_print_first_k_characters.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args)
+	{
+	String str;
+	Scanner s=new Scanner(System.in);
+	str=s.next();
+	int n;
+            n=s.nextInt();
+	String st=str.substring(0,n);
+	System.out.println(st);
+	}
+}
