a95d5bc889764b4a6d0eeeae25cb59cfa0bdb7e7
revakrish266 <revathikrishnan266@gmail.com>
2018-01-18 22:53:36 +0530

    Create number_of_words_in_a_string.java

---
+++ beg/assignments/number_of_words_in_a_string.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+
+{
+	
+public static void main (String[] args) 
+
+	{
+	 
+ String a;
+
+      int count=0;
+
+      Scanner s=new Scanner(System.in);
+
+      a=s.nextLine();
+
+     String d[]=a.split("\\s+");
+ 
+    for(int i=0;i<d.length;i++)
+{
+ 
+        count++;
+
+     }
+
+     System.out.println(count);
+    
+}
+
+	
+}
