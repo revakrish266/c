6724a280fcedce10a15d8e75bf9fbfcf3911d473
revakrish266 <revathikrishnan266@gmail.com>
2018-01-31 22:14:27 +0530

    Update print odd digits present in a number.c

--- beg/assignments/print odd digits present in a number.c
+++ beg/assignments/print odd digits present in a number.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-
 int main(void) {
-		int n,t,rem=0,i;
-    printf("Enter the number");
-    scanf("%d",&n);
-		while(n!=0)
-		{
-			t=n%10;
-			rem=(rem*10)+t;
-			n=n/10;
-		}
-		while(rem!=0)
-		{
-			i=rem%10;
-			rem=rem/10;
-		    if(i%2!=0)
-		    {
-		        printf("%d\n",i);
-		    }
+int n1,t1,rem1=0,i;
+   printf("Enter the number");
+ scanf("%d",&n1);
+while(n1!=0)
+{
+t1=n1%10;
+rem1=(rem1*10)+t1;
+n1=n1/10;
+}
+while(rem1!=0)
+	{
+i=rem1%10;
+rem1=rem1/10;
+  if(i%2!=0)
+   {
+     printf("%d\n",i);
+	    }
