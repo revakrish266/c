ec677e80a8106fb2e91093f0e2494a148becbe0e
revakrish266 <revathikrishnan266@gmail.com>
2018-02-04 02:12:11 +0530

    Create printing_last_n_characters.c

---
+++ beg/assignments/printing_last_n_characters.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int num,len,p,i;
+	char str[100];
+    scanf("%s",str);
+	scanf("%d",&num);
+	len=strlen(str);
+	p=l-num;
+	for(i=p;i<l;i++)
+	{
+		printf("%c",str[i]);
+	}
+	return(0);	
+}
