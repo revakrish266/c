64e93568fc95bafa94cfeee342e2c1de3cf59f86
revakrish266 <revathikrishnan266@gmail.com>
2018-02-04 14:47:34 +0530

    Create printing_first_letter_in_uppercase.c

---
+++ beg/assignments/printing_first_letter_in_uppercase.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main(void) {
+char str[100];
+int len,i;
+gets(str);
+len=strlen(str);
+for(i=0;i<len;i++)
+{
+                if(i==0)
+                {
+                str[0]=toupper(str[0]);
+                }
+                if(str[i]==' ')
+                {
+                str[i+1]=toupper(str[i+1]);
+                }
+}
+printf("%s",str);
+return (0);
+}
